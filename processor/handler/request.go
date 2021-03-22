865617874dbc278fd7f14bf747e134d0b423c40a
Lucas França <lucasfranca.dev@gmail.com>
2021-03-21 23:33:41 -0300

    Test example

--- processor/handler/request.go
+++ processor/handler/request.go
@@ -3,7 +3,6 @@ package handler
 // Request Request
 type Request interface {
 	GetPayload() []byte
-	GetOriginalPayload() []byte
 	GetAttributes() map[string]interface{}
 	GetAttribute(string) interface{}
 }
--- processor/http/processor.go
+++ processor/http/processor.go
@@ -3,11 +3,17 @@ package http
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/lffranca/bento/processor/handler"
 )
 
-type Processor struct{}
+type Processor struct {
+	URL         string
+	QueryParams map[string]string
+	Headers     map[string]string
+	Cookies     []*http.Cookie
+}
 
 func (item *Processor) Handler(context.Context, handler.Request) (handler.Response, error) {
 	log.Println("HTTP HANDLER")
